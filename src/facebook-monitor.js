1dce1508e5460fd464029a9cef6ce7dc39ca709a
breckenridmj <breckenridmj@vcu.edu>
2024-03-13 11:38:14 -0400

    Updated People Click function

--- src/facebook-monitor.js
+++ src/facebook-monitor.js
@@ -55,38 +55,74 @@ async function signIn(page){
         console.error("Error while clicking the button:", error);
     }
 
-    //capture screenshot
+    // Capture Screenshot of Finished Process
     await page.screenshot({ //capture screenshot
         path: './screenshots/LoggedIn.png'
     });
 }
 
 async function searchInput(page){
+    try {
+        // Wait for the search input field to appear
+        await page.waitForSelector('[placeholder="Search Facebook"]');
 
-    // Wait for the search input field to appear
-    await page.waitForSelector('[placeholder="Search Facebook"]');
+        // Type into the search input field
+        await page.type('[placeholder="Search Facebook"]', searchQuery);
 
-    // Type into the search input field
-    await page.type('[placeholder="Search Facebook"]', searchQuery);
+        // Press the Enter key after typing
+        await page.keyboard.press('Enter');
 
-    // Press the Enter key after typing
-    await page.keyboard.press('Enter');
-
+    } catch (error) {
+        console.log("Search Failed For: " + searchQuery, error);
+    }
+    
     // Wait for navigation after pressing Enter
     await page.waitForNavigation();
 
-    // Now the page has navigated, you can continue with further actions
-    await page.screenshot({ path: './screenshots/SearchResults.png' });
+    // Capture Screenshot of finished process
+    await page.screenshot({ //capture screenshot
+        path: './screenshots/SearchResults.png'
+    });
 }
 
-async function searchElement(page){
+async function clickPeople(page){
+    try {
+        // Wait for the list items to appear
+        await page.waitForSelector('[role="listitem"]');
+
+        // Get all list items
+        const listItems = await page.$$('[role="listitem"]');
+
+        // Check if there are at least three list items
+        if (listItems.length >= 10) {
+            // Click on the third list item
+            await listItems[2].click();
+        } else {
+            console.log('There are less than three list items.');
+        }
+    } catch (error) {
+        console.log("Search Failed For: " + error);
+    }
     
+    // Wait for navigation after Clicking People
+    await page.waitForNavigation();
+
+    // Wait until page is idle
+    //await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 30000 }); // Wait for up to 30 seconds
+    // Add a delay of 2 seconds
+    //await page.waitForTimeout(10000);
+
+    // Capture Screenshot of finished process
+    await page.screenshot({ //capture screenshot
+        path: './screenshots/PeopleResults.png'
+    });
 }
 
 async function run () {
     var page = await loadPage();
     await signIn(page);
     await searchInput(page);
+    await clickPeople(page);
 
 }
 
