80cfa2e5097657e068c966ad3e40b8f2c548c32e
breckenridmj <breckenridmj@vcu.edu>
2024-03-19 11:50:22 -0400

    Working on the autoscroll time out error this is just a update

--- src/facebook-monitor.js
+++ src/facebook-monitor.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 const { scheduleJob } = require('node-schedule');
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
 
 // Define the hours variable with a value
 const hours = 1; // Example: run every hour
@@ -23,9 +26,10 @@ const searchQuery = searchName;
 async function loadPage(){
     // Launches browser and disables notification pop up in chrome
     let browser = await puppeteer.launch({
-        headless:false,
+        headless: false,
         defaultViewport: null, // Set viewpot to null
-        timeout: 0, // Set to 0  for no timeout (not reccomended for production change later)
+        // Set to 0  for no timeout (not reccomended for production change later) or 60000 is equal to 60 secs
+        timeout: 3600000  // Set the timeout to 1 hour (3600000 milliseconds)
         //args: ['--no-startup-window'],
         //args: ['--disable-features=site-per-process'], //used for error frame detached
     });
@@ -125,17 +129,20 @@ async function clickPeople(page){
 }
 
     
-async function scrollPageToBottom(page) {
-    console.log("Scrolling to the bottom of the page");
+async function autoScroll(page) {
+
+    console.log("Scrolling and collecting data from the page please wait");
 
     try {
         // Scroll loop
+        console.log("Scroll Started!");
         while (true) {
             // Scroll to the bottom of the page
             await page.evaluate(async () => {
                 await new Promise((resolve, reject) => {
                     let totalHeight = 0;
-                    const distance = 20;  // Increase the distance scrolled per interval
+                    // typical scroll distance for a human user varies but is often around 100 to 200 pixels per scroll
+                    const distance = 100;  // Increase the distance scrolled per interval
                     const scrollInterval = setInterval(() => {
                         const scrollHeight = document.body.scrollHeight;
                         window.scrollBy(0, distance);
@@ -144,29 +151,42 @@ async function scrollPageToBottom(page) {
                             clearInterval(scrollInterval);
                             resolve();
                         }
-                    }, 200); // Adjust the interval as needed
+                        // common interval might be between 200 to 500 milliseconds.
+                    }, 400); // Adjust the interval as needed
                 });
             });
 
-            console.log("Scrolled to the bottom");
+            console.log("Still Scrolling........");
 
-            // Collect search feed content after each scroll
-            const searchFeedContent = await page.$$eval('div[role="feed"]', feed => feed.map(feed => feed.textContent));
+            // // Collect search feed content after each scroll
+            // const searchFeedContent = await page.$$eval('div[role="feed"]', feed => feed.map(feed => feed.textContent));
 
-            // Output search feed content line by line
-            console.log('Search Feed Content:');
-            for (const item of searchFeedContent) {
-                console.log(item);
-            }
+            // // Output search feed content line by line
+            // console.log('Search Feed Content:');
+            // for (const item of searchFeedContent) {
+            //     console.log(item);
+            // }
 
             // Check if the last scroll reached the bottom
-            const isEndOfPage = await page.evaluate(() => {
-                return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+            // const isEndOfPage = await page.evaluate(() => {
+            //     return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+            // });
+
+            // // If reached the bottom, break the loop
+            // if (isEndOfPage) {
+            //     console.log("Reached the bottom of the page");
+            //     break;
+            // }
+
+            // Check if the text "End of results" is found on the page
+            const endOfResultsTextFound = await page.evaluate(() => {
+                const endOfResultsElement = document.querySelector('span[dir="auto"]:contains("End of results")');
+                return endOfResultsElement !== null;
             });
 
-            // If reached the bottom, break the loop
-            if (isEndOfPage) {
-                console.log("Reached the bottom of the page");
+            // If "End of results" is found, break the loop
+            if (endOfResultsTextFound) {
+                console.log("Reached the end of results");
                 break;
             }
         }
@@ -175,51 +195,8 @@ async function scrollPageToBottom(page) {
     }
 }
     
-// Grab Account Info
-async function collectAccountData(page){
 
-    // Collect search feed content after each scroll
-    const searchFeedContent = await page.$$eval('div[role="feed"]', feed => feed.map(feed => feed.textContent));
-
-    // Output search feed content line by line
-    console.log('Search Feed Content:');
-    for (const item of searchFeedContent) {
-        console.log(item);
-    }
-
-
-    // Wait for search feed
-    //await page.waitForNavigation("div[class='.x9f619 .x1n2onr6 .x1ja2u2z .x78zum5 .xdt5ytf .x1iyjqo2 .x2lwn1j']");
-    await page.waitForSelector('div[role="feed"]');
-
-    // Grab all items and map them to accounts then return the objects
-    try {
-        const profiles = await page.evaluate(() => {
-
-            // > div : grab the next div
-            // > div:first-child : grab the first child of that div
-            //
-            //const accounts = Array.from(document.querySelectorAll("div[class='x9f619 x1n2onr6 x1ja2u2z x78zum5 xdt5ytf x1iyjqo2 x2lwn1j'] > div > div > div > div > div> div > div > div:first-child > div")).map( 
-            const accounts = Array.from(document.querySelectorAll('div[role="feed"] > div > div > div > div > div > div > div')).map(
-                accounts => {
-                    return {
-                        // return the name (paragraph) ? if not available return nothing
-                        Name: accounts.querySelector("p") ? accounts.querySelector("p").innerText : ""
-                    };
-                }
-            );
-            return accounts;
-        });
-
-        console.log(profiles);
-    } catch (error) {
-             console.log("Account Capture " + error);
-    }
-
-
-}
-
-
+// Collect Account data from Page
 async function collectAccountData(page){
 
     // Collect search feed content after each scroll
@@ -267,12 +244,69 @@ async function collectAccountData(page){
         // Return Data to the console log
         console.log(data);
 
+        // Convert data to CSV format
+        const csvData = data.map(account => {
+            return `${account.Name},${account.Bio},${account.Link},${account.Profile_Picture}`;
+        }).join('\n');
+
+        // Write CSV data to a file
+        const outputDir = path.join(__dirname, '..', 'accountData');
+        const outputPath = path.join(outputDir, 'output.csv');
+        fs.writeFileSync(outputPath, csvData);
+
+        console.log(`CSV file saved at ${outputPath}`);
+
     } catch (error) {
 
         console.log("Failed to process account profiles", error);
 
     } 
 }
+
+// async function fetchAccountData(page){
+
+//     // Process update
+//     console.log("Attempting to fetch user profiles");
+
+//     // Wait for search feed
+//     await page.waitForSelector('div[role="feed"]');
+
+//     // Grab all items and map them to accounts then return the objects
+//     try {
+//         // Wait for the feed container to load
+//         await page.waitForSelector('div[role="feed"]');
+
+//         // Evaluate in the context of the page to collect account data
+//         await page.evaluate(() => {
+//             // Get all div elements within the feed container
+//             const divs = document.querySelectorAll('div[role="feed"] > div.x1yztbdb');
+
+//             // Iterate over each div element and extract account data
+//             divs.forEach(div => {
+//                 const ariaLabelElement = div.querySelector('[aria-label]');
+//                 const spanTextElement = div.querySelector('span.x1lliihq.x6ikm8r.x10wlt62.x1n2onr6');
+//                 const spanText = spanTextElement ? spanTextElement.textContent : null;
+//                 const hrefElement = div.querySelector('[href]');
+//                 const href = hrefElement ? hrefElement.href : null;
+//                 const profileImageElement = div.querySelector('image');
+//                 const profileImageSrc = profileImageElement ? profileImageElement.getAttribute('xlink:href') : null;
+
+//                 // If the aria-label element is found, log the account data
+//                 if (ariaLabelElement) {
+//                     console.log({
+//                         Name: ariaLabelElement.getAttribute('aria-label'),
+//                         Bio: spanText,
+//                         Link: href,
+//                         Profile_Picture: profileImageSrc,
+//                     });
+//                 }
+//             });
+//         });
+
+//     } catch (error) {
+//         console.log("Failed to collect account data", error);
+//     }
+// }
 
 async function run () {
     var page = await loadPage();
@@ -295,11 +329,16 @@ async function run () {
     // Add a delay of 3 seconds (3000 milliseconds)
     await new Promise(resolve => setTimeout(resolve, 5000));
 
+    //await fetchAccountData(page);
+
+    // Auto Scroll Function
+    await autoScroll(page);
+
     // Collect Account Data Function
     await collectAccountData(page);
 
     // Scroll Function
-    //await scrollPageToBottom(page);
+    // await scrollPageToBottom(page);
 
     // Add a delay of 3 seconds (3000 milliseconds)
     //await new Promise(resolve => setTimeout(resolve, 5000));
